fda1245dccdd29bf32a9f655c369a45a83ac1210
beepboopdefi <beepboop_defi@protonmail.com>
2022-02-18 14:12:44 +0100

    Fixing types

--- src/components/HomepageFeatures.tsx
+++ src/components/HomepageFeatures.tsx
@@ -1,40 +1,43 @@
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import useBaseUrl from "@docusaurus/useBaseUrl";
 import Link from "@docusaurus/Link";
-import React from 'react';
-import clsx from 'clsx';
-import styles from './HomepageFeatures.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./HomepageFeatures.module.css";
 
 type FeatureItem = {
   title: string;
   image: string;
   description: JSX.Element;
+  url: string;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'What is Gearbox',
-    image: '/img/gearbox_man.png',
-    url: 'docs/fundaments/intro',
+    title: "What is Gearbox",
+    image: "/img/gearbox_man.png",
+    url: "docs/fundaments/intro",
     description: (
       <>
-        Learn about the core concepts of Gearbox, its definitions, smart contracts and parameters
+        Learn about the core concepts of Gearbox, its definitions, smart
+        contracts and parameters
       </>
     ),
   },
   {
-    title: 'Set up a dev environment',
-    image: '/img/gearbox_contracts.png',
-    url: 'docs/tutorials/intro',
+    title: "Set up a dev environment",
+    image: "/img/gearbox_contracts.png",
+    url: "docs/tutorials/intro",
     description: (
       <>
-        Get from zero to hero by setting up a local development environment with our step by step tutorials
+        Get from zero to hero by setting up a local development environment with
+        our step by step tutorials
       </>
     ),
   },
   {
-    title: 'Discover our SDK',
-    image: '/img/gearbox_men.png',
-    url: 'docs/sdk/intro',
+    title: "Discover our SDK",
+    image: "/img/gearbox_men.png",
+    url: "docs/sdk/intro",
     description: (
       <>
         Dive into our SDK and learn how it can accelerate your dApp development
@@ -43,21 +46,21 @@ const FeatureList: FeatureItem[] = [
   },
 ];
 
-function Feature({title, image, description, url}: FeatureItem) {
+function Feature({ title, image, description, url }: FeatureItem) {
   return (
-    <div className={clsx('col col--4')}>
-      <Link to={url} class="navbar__link">
-      <div className="text--center">
-        <img
-          className={styles.featureSvg}
-          alt={title}
-          src={useBaseUrl(image)}
-        />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
+    <div className={clsx("col col--4")}>
+      <Link to={url} className="navbar__link">
+        <div className="text--center">
+          <img
+            className={styles.featureSvg}
+            alt={title}
+            src={useBaseUrl(image)}
+          />
+        </div>
+        <div className="text--center padding-horiz--md">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
       </Link>
     </div>
   );
